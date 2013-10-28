fa94fdec4ffc631b186fb85bfaf69440792b2131
Amri <Amri@UPJ-Khusnul>
2013-10-28 16:24:36 +0700

    master

---
+++ application/views/template_groundwork/body_header_default.php
@@ -0,0 +1,11 @@
+  <body>
+    <header class="padded">
+      <div class="container">
+        <div class="row">
+          <div class="one half">
+            <h2 class="logo"><a href="<?php echo base_url()?>" target="_parent"><img src="<?php echo base_url('assets/groundwork')?>/images/groundwork-logo.svg" alt="GroundworkCSS">Attendance I.S</a></h2>
+          </div>
+          <div class="one half">
+              <p class="double pad-top no-pad-small-tablet align-right align-left-small-tablet">&nbsp;</p>
+          </div>
+        </div>
---
+++ application/views/user/login_form_body_header.php
@@ -0,0 +1,8 @@
+  <body>
+    <header class="padded">
+      <div class="container">
+        <div class="row">
+          <div class="one half centered align-center">
+            <h2 class="logo"><a href="<?php echo base_url()?>" target="_parent"><img src="<?php echo base_url('assets/groundwork')?>/images/groundwork-logo.svg" alt="GroundworkCSS">Attendance I.S</a></h2>
+          </div>
+        </div>
